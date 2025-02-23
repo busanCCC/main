61878ead7fb87c71ae6fb2457c10b0b312aeeea4
KJWGarden <mike03131313@gmail.com>
2025-02-23 23:12:52 +0900

    fix: register 페이지 middleware 적용 register 접근 임시 차단. middleware 추가 적용

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,7 @@ export async function middleware(req: NextRequest) {
   } = await supabase.auth.getUser();
 
   // ✅ 인증이 필요한 페이지 보호 (예: `/dashboard` 접근 제한)
-  if (!user && req.nextUrl.pathname.startsWith("/admin-page")) {
+  if (!user && (req.nextUrl.pathname.startsWith("/admin-page") || req.nextUrl.pathname.startsWith("/register"))) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
@@ -22,5 +22,5 @@ export async function middleware(req: NextRequest) {
 
 // ✅ `matcher`를 사용하여 특정 경로에만 미들웨어 적용 가능
 export const config = {
-  matcher: ["/admin-page/:path*"], // `/admin-page` 이하 모든 경로에서 적용
+  matcher: ["/admin-page/:path*", "/register/:path*"], // `/admin-page` 이하 모든 경로에서 적용
 };
